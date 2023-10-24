a09fad91e61f58eb0ec73872d28557fff1719a42
aurelien <ahennuyer@datainmotion.fr>
2023-10-24 11:26:13 +0200

    🔥 unsplash image remote access

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@ const { i18n } = require('./next-i18next.config.js');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	i18n,
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'images.unsplash.com'
-			}
-		]
-	},
 	experimental: {
 		serverActions: true
 	}
